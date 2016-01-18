c35ae3ceb251d1993b6169b2168c990c19e686b1
Carl Venezia <veneziacarl@gmail.com>
2016-01-17 19:51:03 -0500

    altered schedule model validations to have inclusion

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,13 +2,17 @@ class Schedule < ActiveRecord:Base
   belongs_to :habit
 
   validates :habit, presence: true
-  validates :status, presence: true
-  validates :type, presence: true
+  validates :status, presence: true,
+    inclusion: { in: ["do", "dont", "completed", "missed"] }
+  validates :type, presence: true,
+    inclusion: { in: ["day"] }
   validates :repeat, presence: true, inclusion: { in: [true, false] }
   validates :date, presence: true
   validate :date_is_valid_datetime
 
   def date_is_valid_datetime
-    errors.add(:date, 'must be a valid datetime') if ((DateTime.parse(date) rescue ArgumentError) == ArgumentError)
+    if ((DateTime.parse(date) rescue ArgumentError) == ArgumentError)
+      errors.add(:date, 'must be a valid datetime')
+    end
   end
 end
