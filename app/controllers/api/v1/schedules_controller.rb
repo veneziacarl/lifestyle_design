7d1ad600d37668c95d02bd66b4992ac36184bf3c
Carl Venezia <veneziacarl@gmail.com>
2016-01-21 15:52:08 -0500

    new schedule is made when action is taken on displayed schedule for any schedule that has repeat set to TRUE. Changed the Schedule#index controller to display only dates before a week from the current date.

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SchedulesController < Api::V1::BaseController
   end
 
   def index
-    schedules = Schedule.where(status: "do").order(id: :desc)
+    schedules = Schedule.where(status: "do").where("date < ?", (Date.today + 7.days)).order(id: :desc)
     render(
       json: ActiveModel::ArraySerializer.new(
         schedules,
@@ -37,6 +37,9 @@ class Api::V1::SchedulesController < Api::V1::BaseController
 
   def destroy
     schedule = Schedule.find(params[:id])
+    if schedule.repeat
+      create_next_schedule(schedule)
+    end
     Schedule.destroy(schedule.id)
     render(json: Api::V1::ScheduleSerializer.new(schedule))
   end
@@ -49,6 +52,9 @@ class Api::V1::SchedulesController < Api::V1::BaseController
 
   def completed
     schedule = Schedule.find(params[:id])
+    if schedule.repeat
+      create_next_schedule(schedule)
+    end
     schedule.status = "completed"
     updated_schedule = update_info(schedule, schedule_params)
     render(json: Api::V1::ScheduleSerializer.new(updated_schedule))
@@ -56,6 +62,9 @@ class Api::V1::SchedulesController < Api::V1::BaseController
 
   def missed
     schedule = Schedule.find(params[:id])
+    if schedule.repeat
+      create_next_schedule(schedule)
+    end
     schedule.status = "missed"
     updated_schedule = update_info(schedule, schedule_params)
     render(json: Api::V1::ScheduleSerializer.new(updated_schedule))
@@ -63,12 +72,12 @@ class Api::V1::SchedulesController < Api::V1::BaseController
 
   private
 
-  def to_boolean(str)
-    str == 'true'
-  end
-
   def schedule_params
     params.permit(:id, :date, :status, :repeat, :frequency, :title, :description, :goal, :note, :updatedStatus)
+  end
+
+  def to_boolean(str)
+    str == 'true'
   end
 
   def update_info(schedule, info)
@@ -80,5 +89,22 @@ class Api::V1::SchedulesController < Api::V1::BaseController
       flash[:color]= "invalid"
     end
     schedule
+  end
+
+  def create_next_schedule(current_schedule)
+    new_schedule = Schedule.new(
+      date: add_days(current_schedule.date, 7), status: current_schedule.status,
+      repeat: current_schedule.repeat, frequency: current_schedule.frequency,
+      habit: current_schedule.habit
+    )
+    if new_schedule.save
+      return new_schedule
+    else
+      return new_schedule.errors
+    end
+  end
+
+  def add_days(current_date, days)
+    current_date + days.days
   end
 end
